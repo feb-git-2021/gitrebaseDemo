66ffb2db7cde544c4975c7701e4163c6866e6325
Vranda Daga <amit.mishra@zensar.com>
2020-06-04 10:15:03 +0530

    Final commit on Solution
    First commit on solution branch
    
    Second commit on solution branch
    
    Third commit on solution

---
+++ index.js
@@ -0,0 +1,9 @@
+let arr =[3,4,6,7,9,10]
+let res = arr.map(i=>i%2===0)
+console.log(res)
+
+const add= (a,b)=>a +b
+console.log(`Addition is ${add(5,6)}`)
+
+const product= (a,b)=>a * b
+console.log(`product is ${product(5,6)}`)
